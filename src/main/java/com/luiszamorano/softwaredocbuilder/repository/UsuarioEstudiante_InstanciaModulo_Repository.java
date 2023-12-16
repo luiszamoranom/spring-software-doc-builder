36fe33a5ef85610c11a63cc489baa10a185cf246
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-12-16 16:27:07 -0300

    feat(usuarioestudiante_instanciamodulo): servicio

--- src/main/java/com/luiszamorano/softwaredocbuilder/repository/UsuarioEstudiante_InstanciaModulo_Repository.java
+++ src/main/java/com/luiszamorano/softwaredocbuilder/repository/UsuarioEstudiante_InstanciaModulo_Repository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UsuarioEstudiante_InstanciaModulo_Repository extends JpaRepository<UsuarioEstudiante_InstanciaModulo, Long> {
-    List<UsuarioEstudiante_InstanciaModulo_Repository> findByUsuarioAndInstanciaModulo(Usuario usuario, InstanciaModulo instanciaModulo);
-    List<UsuarioEstudiante_InstanciaModulo_Repository> findByUsuario(Usuario usuario);
-    List<UsuarioEstudiante_InstanciaModulo_Repository> findByInstancia(InstanciaModulo instanciaModulo);
+    List<UsuarioEstudiante_InstanciaModulo> findByUsuarioAndInstanciaModulo(Usuario usuario, InstanciaModulo instanciaModulo);
+    List<UsuarioEstudiante_InstanciaModulo> findByUsuario(Usuario usuario);
+    List<UsuarioEstudiante_InstanciaModulo> findByInstanciaModulo(InstanciaModulo instanciaModulo);
 }
---
+++ src/main/java/com/luiszamorano/softwaredocbuilder/service/relation/UsuarioEstudiante_InstanciaModulo_Servicio.java
@@ -0,0 +1,37 @@
+package com.luiszamorano.softwaredocbuilder.service.relation;
+
+import com.luiszamorano.softwaredocbuilder.entity.InstanciaModulo;
+import com.luiszamorano.softwaredocbuilder.entity.Usuario;
+import com.luiszamorano.softwaredocbuilder.entity.relation.UsuarioEstudiante_InstanciaModulo;
+import com.luiszamorano.softwaredocbuilder.repository.UsuarioEstudiante_InstanciaModulo_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioEstudiante_InstanciaModulo_Servicio {
+    @Autowired
+    UsuarioEstudiante_InstanciaModulo_Repository usuarioEstudianteInstanciaModuloRepository;
+
+    public List<UsuarioEstudiante_InstanciaModulo> findAll(){
+        return usuarioEstudianteInstanciaModuloRepository.findAll();
+    }
+
+    public List<UsuarioEstudiante_InstanciaModulo> findByUsuario(Usuario usuario){
+        return usuarioEstudianteInstanciaModuloRepository.findByUsuario(usuario);
+    }
+
+    public List<UsuarioEstudiante_InstanciaModulo> findByInstanciaModulo(InstanciaModulo instanciaModulo){
+        return usuarioEstudianteInstanciaModuloRepository.findByInstanciaModulo(instanciaModulo);
+    }
+
+    public List<UsuarioEstudiante_InstanciaModulo> findByUsuarioAndInstanciaModulo(Usuario usuario, InstanciaModulo instanciaModulo){
+        return usuarioEstudianteInstanciaModuloRepository.findByUsuarioAndInstanciaModulo(usuario,instanciaModulo);
+    }
+
+    public Optional<UsuarioEstudiante_InstanciaModulo> findById(Long id){
+        return usuarioEstudianteInstanciaModuloRepository.findById(id);
+    }
+}
