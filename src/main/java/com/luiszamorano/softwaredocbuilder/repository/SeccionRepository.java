1d7adddcc34f22beb91f03136aa6a13a66782d07
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-12-22 05:02:52 -0300

    feat(seccion): controlador

--- src/main/java/com/luiszamorano/softwaredocbuilder/repository/SeccionRepository.java
+++ src/main/java/com/luiszamorano/softwaredocbuilder/repository/SeccionRepository.java
@@ -3,5 +3,5 @@ package com.luiszamorano.softwaredocbuilder.repository;
 import com.luiszamorano.softwaredocbuilder.entity.Seccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeccionRepository extends JpaRepository<Seccion,Integer> {
+public interface SeccionRepository extends JpaRepository<Seccion,String> {
 }
