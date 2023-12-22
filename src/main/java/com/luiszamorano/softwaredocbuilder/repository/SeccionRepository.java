a903b7da7899db24faaa72621b481c4922633879
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-12-22 03:52:11 -0300

    feat(seccion): entidad y seeder actualizado

---
+++ src/main/java/com/luiszamorano/softwaredocbuilder/repository/SeccionRepository.java
@@ -0,0 +1,7 @@
+package com.luiszamorano.softwaredocbuilder.repository;
+
+import com.luiszamorano.softwaredocbuilder.entity.Seccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeccionRepository extends JpaRepository<Seccion,Integer> {
+}
