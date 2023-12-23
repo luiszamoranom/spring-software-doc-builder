5681e9bf08e8c2ca093d63a7ec6ba296476e5840
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-12-23 11:43:08 -0300

    feat(entrada): entidad

---
+++ src/main/java/com/luiszamorano/softwaredocbuilder/entity/nuevasrelaciones/Entrada.java
@@ -0,0 +1,18 @@
+package com.luiszamorano.softwaredocbuilder.entity.nuevasrelaciones;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Entrada {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
