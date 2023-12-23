883fcc760de9c8c4b6477b81343be6bff858ab39
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-12-23 10:32:57 -0300

    feat(instanciamoduloseccion): entidad sin relaciones

---
+++ src/main/java/com/luiszamorano/softwaredocbuilder/entity/InstanciaModuloSeccion.java
@@ -0,0 +1,18 @@
+package com.luiszamorano.softwaredocbuilder.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class InstanciaModuloSeccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
