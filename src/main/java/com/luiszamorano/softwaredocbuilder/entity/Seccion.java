8871209d0e0b47f47ac60ea6c199812508018850
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-12-23 10:31:09 -0300

    fix(seccion): anotaciones entity y data

--- src/main/java/com/luiszamorano/softwaredocbuilder/entity/Seccion.java
+++ src/main/java/com/luiszamorano/softwaredocbuilder/entity/Seccion.java
@@ -1,9 +1,15 @@
 package com.luiszamorano.softwaredocbuilder.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class Seccion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
